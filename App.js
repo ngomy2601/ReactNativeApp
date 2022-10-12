cdeff27f913fd1bc406001de3da56fa444b2484c
ngomy2001 <ngomy2001@gmail.com>
2022-10-13 01:59:32 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, TextInput, Text } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text style={styles.baseText}>Trip Name:</Text>
+      <TextInput style={styles.input} placeholder="Enter the trip name" />
+      <Text style={styles.baseText}>Destination:</Text>
+      <TextInput style={styles.input} placeholder="Enter the destination" />
+      <Text style={styles.baseText}>Date:</Text>
+      <TextInput style={styles.input} placeholder="Enter the date" />
+      <Text style={styles.baseText}>Require Risks Assessment:</Text>
+      <TextInput style={styles.input} placeholder="Enter YES/NO" />
+      <Text style={styles.baseText}>Description:</Text>
+      <TextInput style={styles.input} placeholder="Enter the description" />
+    </SafeAreaView>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 5,
+  },
+  baseText: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    fontFamily: 'Cochin',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
+
+export default App;
