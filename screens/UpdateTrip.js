3c3063fe87ee45ee64b4d596f4407cf98459c372
ngomy2001 <ngomy2001@gmail.com>
2022-10-13 02:15:12 +0700

    setup-update-layout

---
+++ screens/UpdateTrip.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Text,
+  Button,
+} from 'react-native';
+
+const UpdateTrip = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.titleText}>UPDATE TRIP</Text>
+      <Text style={styles.baseText}>Trip Name:</Text>
+      <TextInput style={styles.input} placeholder="Enter the trip name" />
+      <Text style={styles.baseText}>Destination:</Text>
+      <TextInput style={styles.input} placeholder="Enter the destination" />
+      <Text style={styles.baseText}>Date:</Text>
+      <TextInput style={styles.input} placeholder="Enter the date" />
+      <Text style={styles.baseText}>Require Risks Assessment:</Text>
+      <TextInput style={styles.input} placeholder="Enter YES/NO" />
+      <Text style={styles.baseText}>Description:</Text>
+      <TextInput style={styles.input} placeholder="Enter the description" />
+      <Button title="UPDATE" color="#0000ff" />
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 5,
+  },
+  baseText: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    fontFamily: 'Cochin',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  titleText: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'crimson',
+  },
+});
+
+export default UpdateTrip;
