a16e3e93834a7e78b5c689c73faba149b66ddff8
ngomy2001 <ngomy2001@gmail.com>
2022-10-17 21:58:14 +0700

    display-trips

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,6 @@
 import * as SQLite from 'expo-sqlite';
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, Button } from 'react-native';
+import { FlatList, Text, View, SafeAreaView, Button } from 'react-native';
 
 const openDatabase = () => {
   const myDB = SQLite.openDatabase('database.db');
@@ -9,6 +9,8 @@ const openDatabase = () => {
 
 const myDB = openDatabase();
 const Home = ({ navigation }) => {
+  let [flatListItems, setFlatListItems] = useState([]);
+
   const createTripTable = () => {
     myDB.transaction((tx) => {
       tx.executeSql(
@@ -27,14 +29,55 @@ const Home = ({ navigation }) => {
       );
     });
   };
+
+  const getTripData = () => {
+    myDB.transaction((tx) => {
+      tx.executeSql('SELECT * FROM table_trip', [], (tx, results) => {
+        var temp = [];
+        for (let i = 0; i < results.rows.length; ++i)
+          temp.push(results.rows.item(i));
+        setFlatListItems(temp);
+      });
+    });
+  };
+  let listViewItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 0.2,
+          width: '100%',
+          backgroundColor: '#808080',
+        }}
+      />
+    );
+  };
+  let listItemView = (item) => {
+    return (
+      <View key={item._id} style={{ backgroundColor: 'white', padding: 20 }}>
+        <Text>Id: {item._id}</Text>
+        <Text>Name: {item.trip_name}</Text>
+        <Text>Destination: {item.trip_destination}</Text>
+        <Text>Datetime: {item.datetime}</Text>
+        <Text>Require Assessment*: {item.trip_assessment}</Text>
+        <Text>Description: {item.trip_description}</Text>
+      </View>
+    );
+  };
   useEffect(() => {
     createTripTable();
+    getTripData();
   }, []);
   return (
     <SafeAreaView>
       <Button
         title="Add a new trip"
         onPress={() => navigation.navigate('AddTrip')}
+      />
+      <FlatList
+        data={flatListItems}
+        ItemSeparatorComponent={listViewItemSeparator}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => listItemView(item)}
       />
     </SafeAreaView>
   );
